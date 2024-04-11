fc14c57c5bb02f5413b6e6223b1fd317dd510c39
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-11 02:36:46 +0100

    Add test objects

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,7 +6,15 @@ namespace ColorSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Color c = new Color(240,0,7);
+            Sphere s = new Sphere(c,15);
+
+            s.Throw();
+            s.Throw();
+            s.Pop();
+            s.Throw();
+            Console.WriteLine(s.GetTimesThrown());
+            Console.WriteLine(c);
         }
     }
 }
