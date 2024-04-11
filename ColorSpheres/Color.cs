0fc76bfcb5562f5784dc7ff92454267df5810a69
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-11 02:23:45 +0100

    Add GetGrey()

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,49 @@ namespace ColorSpheres
 {
     public class Color
     {
-        
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        } 
+        public Color(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
+
+        public byte GetRed()
+        {
+            return red;
+        }
+        public byte GetGreen()
+        {
+            return green;
+        }
+        public byte GetBlue()
+        {
+            return blue;
+        }
+        public byte GetAlpha()
+        {
+            return alpha;
+        }
+        public byte GetGrey()
+        {
+            int redInt = Convert.ToInt32(red);
+            int greenInt = Convert.ToInt32(green);
+            int blueInt = Convert.ToInt32(blue);
+            byte grey = Convert.ToByte((redInt + greenInt + blueInt)/3);
+
+            return grey;
+        }
     }
 }
