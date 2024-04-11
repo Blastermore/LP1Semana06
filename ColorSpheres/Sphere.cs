67da7005ecd52943f727041a90beda3ed63e677c
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-11 02:32:15 +0100

    Add Sphere Class; Add Pop() method; Add Throw(); GetTimesThrown()

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Sphere
+    {
+        private Color color;
+        private int radius;
+        private int thrown;
+        public Sphere(Color color, int radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            thrown = 0;
+        }
+        public void Pop()
+        {
+            radius = 0;
+        }
+        public void Throw()
+        {
+            if (radius > 0)
+            {
+                thrown += 1;
+            }
+        }
+        public int GetTimesThrown()
+        {
+            return thrown;
+        }
+    }
+}
